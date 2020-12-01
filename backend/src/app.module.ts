49117333e2448260a73956b4248ba42711312f2a
Jinhyo <sosilion@gmail.com>
2020-12-01 22:38:50 +0900

    3. typeorm setting, ConfigModule setting

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { RetaurantsModule } from './retaurants/retaurants.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+      }),
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'), // or true
     }),
+    TypeOrmModule.forRoot(),
     RetaurantsModule,
   ],
   controllers: [],
