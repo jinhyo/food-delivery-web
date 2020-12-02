4f474f86eb0fa3ffcc25fe72308e2ae4c604b3c4
Jinhyo <sosilion@gmail.com>
2020-12-02 10:26:55 +0900

    4. db entity setting

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { join } from 'path';
 import { RetaurantsModule } from './retaurants/retaurants.module';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
+import { Restaurant } from './retaurants/entities/restaurant.entity';
 
 @Module({
   imports: [
@@ -14,12 +15,27 @@ import * as Joi from 'joi';
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
       validationSchema: Joi.object({
         NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
       }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: process.env.NODE_ENV !== 'prod',
+      logging: false,
+      entities: [Restaurant],
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'), // or true
     }),
-    TypeOrmModule.forRoot(),
     RetaurantsModule,
   ],
   controllers: [],
--- backend/src/retaurants/entities/restaurant.entity.ts
+++ backend/src/retaurants/entities/restaurant.entity.ts
@@ -1,17 +1,27 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
+  @Field(() => Number)
+  @PrimaryGeneratedColumn()
+  id: number;
+
   @Field(() => String)
   // ()안의 인자는 중요하지 않음, Field는 graphQl용
+  @Column()
   name: string;
 
+  @Column()
   @Field(() => Boolean)
   isVegan?: boolean;
 
+  @Column()
   @Field(() => String)
   address: string;
 
+  @Column()
   @Field(() => String)
   ownerName: string;
 }
