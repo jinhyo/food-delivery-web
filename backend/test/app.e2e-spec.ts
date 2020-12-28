c8b07f0196c074e60fa7a06280e2f356639fadb9
Jinhyo <sosilion@gmail.com>
2020-12-28 13:40:55 +0900

    28.  E2E test: createAccount mutation

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -2,6 +2,15 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from './../src/app.module';
 import { getConnection } from 'typeorm';
+import request from 'supertest';
+
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -20,7 +29,51 @@ describe('AppController (e2e)', () => {
     app.close();
   });
 
-  describe('createAccount mutation', () => {});
+  describe('createAccount mutation', () => {
+    it('계정 생성 성공', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+          createAccount(
+            userInfo: { role: Owner, email: "test@naver.com", password: "12345" }
+          ) {
+            ok
+            error
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it('동일한 이메일이 있을 경우 실패', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+          createAccount(
+            userInfo: { role: Owner, email: "test@naver.com", password: "12345" }
+          ) {
+            ok
+            error
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toEqual(expect.any(String));
+        });
+    });
+  });
   describe('userProfile query', () => {});
   describe('login mutation', () => {});
   describe('me query', () => {});
